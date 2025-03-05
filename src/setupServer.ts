f1f9f114e7441e691d52c452a8945d100e155931
thiendang <thiendh310@gmail.com>
2025-03-05 15:28:40 +0700

    feat: change cookie maxAge

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -38,7 +38,7 @@ export class XServer {
       cookieSession({
         name: 'session',
         keys: [config.SECRET_KEY_ONE!, config.SECRET_KEY_TWO!],
-        maxAge: 5000,
+        maxAge: 24 * 7 * 3600000,
         secure: config.NODE_ENV !== 'development'
       })
     );
